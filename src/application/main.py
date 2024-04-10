e1cba16361a568886ff5f104fceffc3eb434c8a2
NivranshuB <nivran.bose@gmail.com>
2024-04-10 20:07:43 +1200

    Added input for car's mileage and per litre petrol cost

--- src/application/main.py
+++ src/application/main.py
@@ -2,18 +2,23 @@ print('Welcome to the Transport Calculator!')
 print('====================================')
 
 perLitrePetrolCost = 2.7
+
+perLitrePetrolCost = input('\nHow much are you paying per litre of petrol right now? ')
+
 perKmTransportCost = 0.2348
 
 numberOfSavedLocations = 0
 totalWeeklyTransportCosts = 0
 
-petrolMileageForYourCar = 14 #The distance travelled per litre of petrol by the individual's car
+petrolMileageForYourCar = 14 #The distance travelled per litre of petrol by the individual's car (set to default at the start)
+
+petrolMileageForYourCar = input('\nWhat is your car\'s petrol mileage (how many kms it travels per litre)? ')
 
 moreLocationsToAdd = False
 
 print('You have ' + str(numberOfSavedLocations) + ' locations saved currently.')
 
-moreLocationsToAddAnswer = input('\nWould you like to add more locations that you visit? Answer \'Y\' for yes otherwise it will be assumed you travel here using public transport. ')
+moreLocationsToAddAnswer = input('\nWould you like to add more locations that you visit? Answer \'Y\' for yes otherwise no more locations will be added. ')
 if moreLocationsToAddAnswer == 'Y':
     moreLocationsToAdd = True
 else:
@@ -37,7 +42,7 @@ while moreLocationsToAdd:
     weeklyCostToTravelToThisLocation = 0
 
     if (travelByCar):
-        weeklyCostToTravelToThisLocation += weeklyDistanceToTravelToThisLocation/petrolMileageForYourCar * perLitrePetrolCost
+        weeklyCostToTravelToThisLocation += weeklyDistanceToTravelToThisLocation/int(petrolMileageForYourCar) * float(perLitrePetrolCost)
     else:
         weeklyCostToTravelToThisLocation += weeklyDistanceToTravelToThisLocation * perKmTransportCost
 
